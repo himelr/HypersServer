23048f77a821769d3a5519bc401255dd728fb27a
Himel <rebelhaze@gmail.com>
2018-04-18 22:06:40 +0300

    New Boot conf

---
+++ src/main/java/com/example/demo/Application.scala
@@ -0,0 +1,12 @@
+package com.example.demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@SpringBootApplication
+class Application
+
+object Application extends App {
+  SpringApplication.run(classOf[Application]);
+}
